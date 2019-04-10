8e29a42c47b7fae6b505f760c08a76d6c6f84e92
Faisal Uje <ujefaisal@gmail.com>
2019-04-10 17:07:02 +0700

    lecture-163 Header component (navigation)

--- src/components/BlogPostList.js
+++ src/components/BlogPostList.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 class BlogPostList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const { posts } = this.props;
 
