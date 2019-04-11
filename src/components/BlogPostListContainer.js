22b7c77b6f13ea6a86621c684424c5777237fc28
Faisal Uje <ujefaisal@gmail.com>
2019-04-11 11:20:15 +0700

    lecture-166 Route with parameter - individual BlogPost

--- src/components/BlogPostListContainer.js
+++ src/components/BlogPostListContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BlogPostList from "./BlogPostList";
-import { blogPostListFetch, blogPostAdd } from "../actions/actions";
+import { blogPostListFetch } from "../actions/actions";
 import { connect } from "react-redux";
 
 const mapStateToProps = state => ({
@@ -8,20 +8,17 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-    blogPostAdd,
     blogPostListFetch
 }
 
 class BlogPostListContainer extends React.Component {
     componentDidMount() {
-        setTimeout(this.props.blogPostAdd, 3000);
-        setTimeout(this.props.blogPostAdd, 5000);
-        setTimeout(this.props.blogPostAdd, 7000);
         this.props.blogPostListFetch();
     }
 
     render() {
-        return (<BlogPostList posts={this.props.posts} isFetching={this.props.isFetching}/>)
+        const { posts, isFetching } = this.props;
+        return (<BlogPostList posts={posts} isFetching={isFetching}/>)
     }
 }
 
