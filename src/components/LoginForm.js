715686a4db87f0b7d3cba54bbccf48fcdf15acc7
Faisal Uje <ujefaisal@gmail.com>
2019-04-12 16:35:12 +0700

    lecture-174 Introducing redux-form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { reduxForm } from 'redux-form';
 
 class LoginForm extends React.Component {
     render() {
+        console.log(this.props);
         return (<div>Hello from LoginForm!</div>)
     }
 }
 
-export default LoginForm;
+export default reduxForm({
+    form: 'LoginForm'
+})(LoginForm);
